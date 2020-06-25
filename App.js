9083d92f54722bb0e4b32b747eebe209a8bfe7b8
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-06-25 12:08:00 -0300

    initialParams

--- App.js
+++ App.js
@@ -10,20 +10,34 @@ function HomeScreen({ navigation }) {
       <Text>Home Screen</Text>
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', {
+            // itemId: 86,
+            otherParam: 'anything you want here',
+          });
+        }}
       />
     </View>
   );
 }
 
-
-function DetailsScreen({ navigation }) {
+function DetailsScreen({ route, navigation }) {
+  /* 2. Get the param */
+  const { itemId } = route.params;
+  const { otherParam } = route.params;
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
       <Button
         title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
       />
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
@@ -38,7 +52,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} initialParams={{ itemId: 42 }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
