11c61387d0ee4b83529c3a9096440368fa0eb1af
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-06-25 12:00:01 -0300

    function goBack navigation

--- App.js
+++ App.js
@@ -17,10 +17,16 @@ function HomeScreen({ navigation }) {
 }
 
 
-function DetailsScreen() {
+function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
